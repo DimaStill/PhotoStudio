1fb99e5bf723dbadc1ad9e37e862576183e220f9
DimaStill <dimastill123@gmail.com>
2019-05-06 18:49:20 +0300

    Connect command

--- PhotoStudio/MainWindow.xaml.cs
+++ PhotoStudio/MainWindow.xaml.cs
@@ -23,6 +23,31 @@ namespace PhotoStudio
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataContext != null)
+                switch (tabControl.SelectedIndex)
+                {
+                    case 1:
+                        addButton.Command = ((ApplicationViewModel)DataContext).AddOrderCommand;
+                        removeButton.Command = ((ApplicationViewModel)DataContext).RemoveOrderCommand;
+                        removeButton.CommandParameter = ((ApplicationViewModel)DataContext).SelectedOrder;
+                        break;
+                    case 2:
+                        addButton.Command = ((ApplicationViewModel)DataContext).AddClientCommand;
+                        removeButton.Command = ((ApplicationViewModel)DataContext).RemoveClientCommand;
+                        removeButton.CommandParameter = ((ApplicationViewModel)DataContext).SelectedClient;
+                        break;
+                    case 3:
+                        addButton.Command = ((ApplicationViewModel)DataContext).AddServiceCommand;
+                        removeButton.Command = ((ApplicationViewModel)DataContext).RemoveServiceCommand;
+                        removeButton.CommandParameter = ((ApplicationViewModel)DataContext).SelectedService;
+                        break;
+                }
         }
     }
 }
