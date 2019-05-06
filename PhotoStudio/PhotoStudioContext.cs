f021ab41f34a2f1d9d4de214215c4cb4a5c73eeb
DimaStill <dimastill123@gmail.com>
2019-05-06 18:47:25 +0300

    Create DB Context

---
+++ PhotoStudio/PhotoStudioContext.cs
@@ -0,0 +1,16 @@
+﻿using PhotoStudio.Model;
+using System.Data.Entity;
+
+namespace PhotoStudio.Context
+{
+    class PhotoStudioContext : DbContext
+    {
+        public PhotoStudioContext()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
